18b50a00aae6c1e55cf7f41352ff085467b31f97
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-10-22 02:57:12 +0100

    🍱 Added TemTem data

---
+++ src/data/temtem.ts
@@ -0,0 +1,842 @@
+interface TemTem {
+	number: number;
+	name: string;
+	type: [Types] | [Types, Types];
+}
+
+type Types =
+	| 'neutral'
+	| 'wind'
+	| 'earth'
+	| 'water'
+	| 'fire'
+	| 'nature'
+	| 'electric'
+	| 'mental'
+	| 'digital'
+	| 'melee'
+	| 'crystal'
+	| 'toxic'
+
+export const temtemData: TemTem[] = [
+	{
+		number: 1,
+		name: 'Mimit',
+		type: ['digital']
+	},
+	{
+		number: 2,
+		name: 'Oree',
+		type: ['digital']
+	},
+	{
+		number: 3,
+		name: 'Zaobian',
+		type: ['digital']
+	},
+	{
+		number: 4,
+		name: 'Chromeon',
+		type: ['digital']
+	},
+	{
+		number: 5,
+		name: 'Halzhi',
+		type: ['digital']
+	},
+	{
+		number: 6,
+		name: 'Molgu',
+		type: ['digital']
+	},
+	{
+		number: 7,
+		name: 'Platypet',
+		type: ['water', 'toxic']
+	},
+	{
+		number: 8,
+		name: 'Platox',
+		type: ['water', 'toxic']
+	},
+	{
+		number: 9,
+		name: 'Platimous',
+		type: ['water', 'toxic']
+	},
+	{
+		number: 10,
+		name: 'Swali',
+		type: ['nature']
+	},
+	{
+		number: 11,
+		name: 'Loali',
+		type: ['nature', 'wind']
+	},
+	{
+		number: 12,
+		name: 'Tateru',
+		type: ['neutral']
+	},
+	{
+		number: 13,
+		name: 'Gharunder',
+		type: ['toxic', 'electric']
+	},
+	{
+		number: 14,
+		name: 'Mosu',
+		type: ['melee']
+	},
+	{
+		number: 15,
+		name: 'Magmut',
+		type: ['melee', 'fire']
+	},
+	{
+		number: 16,
+		name: 'Paharo',
+		type: ['wind']
+	},
+	{
+		number: 17,
+		name: 'Paharac',
+		type: ['wind']
+	},
+	{
+		number: 18,
+		name: 'Granpah',
+		type: ['wind']
+	},
+	{
+		number: 19,
+		name: 'Ampling',
+		type: ['electric']
+	},
+	{
+		number: 20,
+		name: 'Amphatyr',
+		type: ['electric', 'nature']
+	},
+	{
+		number: 21,
+		name: 'Bunbun',
+		type: ['earth', 'fire']
+	},
+	{
+		number: 22,
+		name: 'Mudrid',
+		type: ['earth', 'fire']
+	},
+	{
+		number: 23,
+		name: 'Hidody',
+		type: ['nature']
+	},
+	{
+		number: 24,
+		name: 'Taifu',
+		type: ['nature']
+	},
+	{
+		number: 25,
+		name: 'Fomu',
+		type: ['water']
+	},
+	{
+		number: 26,
+		name: 'Wilplump',
+		type: ['water', 'wind']
+	},
+	{
+		number: 27,
+		name: 'Skail',
+		type: ['neutral']
+	},
+	{
+		number: 28,
+		name: 'Skunch',
+		type: ['neutral', 'melee']
+	},
+	{
+		number: 29,
+		name: 'Goty',
+		type: ['neutral']
+	},
+	{
+		number: 30,
+		name: 'Mouflank',
+		type: ['neutral']
+	},
+	{
+		number: 31,
+		name: 'Rhoulder',
+		type: ['neutral', 'fire']
+	},
+	{
+		number: 32,
+		name: 'Houchic',
+		type: ['mental']
+	},
+	{
+		number: 33,
+		name: 'Tental',
+		type: ['mental']
+	},
+	{
+		number: 34,
+		name: 'Nagaise',
+		type: ['mental', 'water']
+	},
+	{
+		number: 35,
+		name: 'Orphyll',
+		type: ['nature', 'toxic']
+	},
+	{
+		number: 36,
+		name: 'Nidrasil',
+		type: ['nature', 'toxic']
+	},
+	{
+		number: 37,
+		name: 'Banapi',
+		type: ['fire']
+	},
+	{
+		number: 38,
+		name: 'Capyre',
+		type: ['fire']
+	},
+	{
+		number: 39,
+		name: 'Lapinite',
+		type: ['crystal']
+	},
+	{
+		number: 40,
+		name: 'Azuroc',
+		type: ['crystal']
+	},
+	{
+		number: 41,
+		name: 'Zenoreth',
+		type: ['crystal']
+	},
+	{
+		number: 42,
+		name: 'Reval',
+		type: ['mental']
+	},
+	{
+		number: 43,
+		name: 'Aohi',
+		type: ['mental', 'fire']
+	},
+	{
+		number: 44,
+		name: 'Bigu',
+		type: ['nature']
+	},
+	{
+		number: 45,
+		name: 'Babawa',
+		type: ['nature', 'water']
+	},
+	{
+		number: 46,
+		name: '0b1',
+		type: ['digital', 'electric']
+	},
+	{
+		number: 47,
+		name: '0b10',
+		type: ['digital', 'electric']
+	},
+	{
+		number: 48,
+		name: 'Kaku',
+		type: ['nature']
+	},
+	{
+		number: 49,
+		name: 'Saku',
+		type: ['nature', 'wind']
+	},
+	{
+		number: 50,
+		name: 'Valash',
+		type: ['neutral', 'crystal']
+	},
+	{
+		number: 51,
+		name: 'Towly',
+		type: ['melee', 'mental']
+	},
+	{
+		number: 52,
+		name: 'Owlhe',
+		type: ['wind', 'melee']
+	},
+	{
+		number: 53,
+		name: 'Barnshe',
+		type: ['wind', 'mental']
+	},
+	{
+		number: 54,
+		name: 'Gyalis',
+		type: ['crystal', 'melee']
+	},
+	{
+		number: 55,
+		name: 'Occlura',
+		type: ['crystal']
+	},
+	{
+		number: 56,
+		name: 'Myx',
+		type: ['crystal', 'mental']
+	},
+	{
+		number: 57,
+		name: 'Raiber',
+		type: ['fire']
+	},
+	{
+		number: 58,
+		name: 'Raize',
+		type: ['fire']
+	},
+	{
+		number: 59,
+		name: 'Raican',
+		type: ['fire']
+	},
+	{
+		number: 60,
+		name: 'Pewki',
+		type: ['water']
+	},
+	{
+		number: 61,
+		name: 'Piraniant',
+		type: ['water']
+	},
+	{
+		number: 62,
+		name: 'Scarawatt',
+		type: ['electric']
+	},
+	{
+		number: 63,
+		name: 'Scaravolt',
+		type: ['electric', 'fire']
+	},
+	{
+		number: 64,
+		name: 'Hoglip',
+		type: ['digital', 'fire']
+	},
+	{
+		number: 65,
+		name: 'Hedgine',
+		type: ['digital', 'fire']
+	},
+	{
+		number: 66,
+		name: 'Osuchi',
+		type: ['earth']
+	},
+	{
+		number: 67,
+		name: 'Osukan',
+		type: ['earth', 'melee']
+	},
+	{
+		number: 68,
+		name: 'Osukai',
+		type: ['earth', 'melee']
+	},
+	{
+		number: 69,
+		name: 'Saipat',
+		type: ['water', 'melee']
+	},
+	{
+		number: 70,
+		name: 'Pycko',
+		type: ['earth', 'fire']
+	},
+	{
+		number: 71,
+		name: 'Drakash',
+		type: ['earth', 'fire']
+	},
+	{
+		number: 72,
+		name: 'Crystle',
+		type: ['crystal']
+	},
+	{
+		number: 73,
+		name: 'Sherald',
+		type: ['crystal']
+	},
+	{
+		number: 74,
+		name: 'Tortenite',
+		type: ['crystal', 'toxic']
+	},
+	{
+		number: 75,
+		name: 'Innki',
+		type: ['electric', 'crystal']
+	},
+	{
+		number: 76,
+		name: 'Shaolite',
+		type: ['melee']
+	},
+	{
+		number: 77,
+		name: 'Shaolant',
+		type: ['melee']
+	},
+	{
+		number: 78,
+		name: 'Cycrox',
+		type: ['digital', 'toxic']
+	},
+	{
+		number: 79,
+		name: 'Hocus',
+		type: ['mental']
+	},
+	{
+		number: 80,
+		name: 'Pocus',
+		type: ['mental']
+	},
+	{
+		number: 81,
+		name: 'Smolzy',
+		type: ['electric']
+	},
+	{
+		number: 82,
+		name: 'Sparzy',
+		type: ['electric']
+	},
+	{
+		number: 83,
+		name: 'Golzy',
+		type: ['electric', 'melee']
+	},
+	{
+		number: 84,
+		name: 'Mushi',
+		type: ['toxic']
+	},
+	{
+		number: 85,
+		name: 'Mushook',
+		type: ['toxic', 'melee']
+	},
+	{
+		number: 86,
+		name: 'Magmis',
+		type: ['fire']
+	},
+	{
+		number: 87,
+		name: 'Mastione',
+		type: ['fire', 'water']
+	},
+	{
+		number: 88,
+		name: 'Umishi',
+		type: ['water']
+	},
+	{
+		number: 89,
+		name: 'Ukama',
+		type: ['water']
+	},
+	{
+		number: 90,
+		name: 'Galvanid',
+		type: ['electric']
+	},
+	{
+		number: 91,
+		name: 'Raignet',
+		type: ['electric']
+	},
+	{
+		number: 92,
+		name: 'Smazee',
+		type: ['melee']
+	},
+	{
+		number: 93,
+		name: 'Baboong',
+		type: ['melee']
+	},
+	{
+		number: 94,
+		name: 'Seismunch',
+		type: ['melee', 'earth']
+	},
+	{
+		number: 95,
+		name: 'Zizare',
+		type: ['earth']
+	},
+	{
+		number: 96,
+		name: 'Gorong',
+		type: ['melee']
+	},
+	{
+		number: 97,
+		name: 'Mitty',
+		type: ['mental']
+	},
+	{
+		number: 98,
+		name: 'Sanbi',
+		type: ['mental']
+	},
+	{
+		number: 99,
+		name: 'Momo',
+		type: ['neutral']
+	},
+	{
+		number: 100,
+		name: 'Kuri',
+		type: ['earth']
+	},
+	{
+		number: 101,
+		name: 'Kauren',
+		type: ['earth']
+	},
+	{
+		number: 102,
+		name: 'Spriole',
+		type: ['nature']
+	},
+	{
+		number: 103,
+		name: 'Deendre',
+		type: ['nature']
+	},
+	{
+		number: 104,
+		name: 'Cerneaf',
+		type: ['nature']
+	},
+	{
+		number: 105,
+		name: 'Toxolotl',
+		type: ['toxic']
+	},
+	{
+		number: 106,
+		name: 'Noxolotl',
+		type: ['toxic']
+	},
+	{
+		number: 107,
+		name: 'Blooze',
+		type: ['toxic']
+	},
+	{
+		number: 108,
+		name: 'Goolder',
+		type: ['toxic']
+	},
+	{
+		number: 109,
+		name: 'Zephyruff',
+		type: ['toxic', 'wind']
+	},
+	{
+		number: 110,
+		name: 'Volarend',
+		type: ['toxic', 'wind']
+	},
+	{
+		number: 111,
+		name: 'Grumvel',
+		type: ['earth']
+	},
+	{
+		number: 112,
+		name: 'Grumper',
+		type: ['earth', 'electric']
+	},
+	{
+		number: 113,
+		name: 'Ganki',
+		type: ['electric', 'wind']
+	},
+	{
+		number: 114,
+		name: 'Gazuma',
+		type: ['electric', 'wind']
+	},
+	{
+		number: 115,
+		name: 'Oceara',
+		type: ['water']
+	},
+	{
+		number: 116,
+		name: 'Yowlar',
+		type: ['neutral']
+	},
+	{
+		number: 117,
+		name: 'Droply',
+		type: ['water', 'earth']
+	},
+	{
+		number: 118,
+		name: 'Garyo',
+		type: ['water', 'earth']
+	},
+	{
+		number: 119,
+		name: 'Broccoblin',
+		type: ['nature']
+	},
+	{
+		number: 120,
+		name: 'Broccorc',
+		type: ['nature', 'melee']
+	},
+	{
+		number: 121,
+		name: 'Broccolem',
+		type: ['nature', 'melee']
+	},
+	{
+		number: 122,
+		name: 'Shuine',
+		type: ['crystal', 'water']
+	},
+	{
+		number: 123,
+		name: 'Nessla',
+		type: ['water', 'electric']
+	},
+	{
+		number: 124,
+		name: 'Valiar',
+		type: ['mental']
+	},
+	{
+		number: 125,
+		name: 'Pupoise',
+		type: ['digital', 'nature']
+	},
+	{
+		number: 126,
+		name: 'Loatle',
+		type: ['digital', 'mental']
+	},
+	{
+		number: 127,
+		name: 'Kalazu',
+		type: ['water']
+	},
+	{
+		number: 128,
+		name: 'Kalabyss',
+		type: ['water', 'toxic']
+	},
+	{
+		number: 129,
+		name: 'Adoroboros',
+		type: ['toxic', 'mental']
+	},
+	{
+		number: 130,
+		name: 'Tuwai',
+		type: ['wind']
+	},
+	{
+		number: 131,
+		name: 'Tukai',
+		type: ['wind', 'water']
+	},
+	{
+		number: 132,
+		name: 'Tulcan',
+		type: ['wind', 'fire']
+	},
+	{
+		number: 133,
+		name: 'Tuvine',
+		type: ['wind', 'crystal']
+	},
+	{
+		number: 134,
+		name: 'Turoc',
+		type: ['wind', 'earth']
+	},
+	{
+		number: 135,
+		name: 'Tuwire',
+		type: ['wind', 'digital']
+	},
+	{
+		number: 136,
+		name: 'Tutsu',
+		type: ['wind', 'melee']
+	},
+	{
+		number: 137,
+		name: 'Kinu',
+		type: ['nature', 'mental']
+	},
+	{
+		number: 138,
+		name: 'Vulvir',
+		type: ['fire', 'earth']
+	},
+	{
+		number: 139,
+		name: 'Vulor',
+		type: ['fire', 'earth']
+	},
+	{
+		number: 140,
+		name: 'Vulcrane',
+		type: ['fire', 'earth']
+	},
+	{
+		number: 141,
+		name: 'Pigepic',
+		type: ['wind']
+	},
+	{
+		number: 142,
+		name: 'Akranox',
+		type: ['earth', 'toxic']
+	},
+	{
+		number: 143,
+		name: 'Koish',
+		type: ['water'] // may have random secondary type
+	},
+	{
+		number: 144,
+		name: 'Vulffy',
+		type: ['earth', 'nature']
+	},
+	{
+		number: 145,
+		name: 'Chubee',
+		type: ['digital', 'wind']
+	},
+	{
+		number: 146,
+		name: 'Waspeen',
+		type: ['digital', 'crystal']
+	},
+	{
+		number: 147,
+		name: 'Mawtle',
+		type: ['digital', 'nature']
+	},
+	{
+		number: 148,
+		name: 'Mawmense',
+		type: ['digital', 'nature']
+	},
+	{
+		number: 149,
+		name: 'Hazrat',
+		type: ['toxic', 'fire']
+	},
+	{
+		number: 150,
+		name: 'Minttle',
+		type: ['mental']
+	},
+	{
+		number: 151,
+		name: 'Minox',
+		type: ['mental', 'electric']
+	},
+	{
+		number: 152,
+		name: 'Minothor',
+		type: ['mental', 'electric']
+	},
+	{
+		number: 153,
+		name: 'Maoala',
+		type: ['melee', 'mental']
+	},
+	{
+		number: 154,
+		name: 'Venx',
+		type: ['neutral']
+	},
+	{
+		number: 155,
+		name: 'Venmet',
+		type: ['neutral', 'melee']
+	},
+	{
+		number: 156,
+		name: 'Vental',
+		type: ['neutral', 'mental']
+	},
+	{
+		number: 157,
+		name: 'Chimurian',
+		type: ['nature', 'crystal']
+	},
+	{
+		number: 158,
+		name: 'Arachnyte',
+		type: ['neutral', 'digital']
+	},
+	{
+		number: 159,
+		name: 'Thaiko',
+		type: ['digital', 'melee']
+	},
+	{
+		number: 160,
+		name: 'Monkko',
+		type: ['digital', 'melee']
+	},
+	{
+		number: 161,
+		name: 'Anahir',
+		type: ['crystal', 'fire']
+	},
+	{
+		number: 162,
+		name: 'Anatan',
+		type: ['crystal', 'fire']
+	},
+	{
+		number: 163,
+		name: 'Tyranak',
+		type: ['fire', 'nature']
+	},
+	{
+		number: 164,
+		name: 'Volgon',
+		type: ['electric']
+	}
+]
